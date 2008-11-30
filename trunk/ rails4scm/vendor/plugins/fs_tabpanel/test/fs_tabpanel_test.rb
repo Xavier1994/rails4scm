701e5e2825fb6232c397fcb0dba54a95fbbaf5d7
fuxinqi@sina.com <fuxinqi@sina.com@cc345c39-9238-0410-bb01-f3a6c2eead41>
2008-11-30 07:38:22 +0000

    增加测试用例。 如何测试？

--- trunk/ rails4scm/vendor/plugins/fs_tabpanel/test/fs_tabpanel_test.rb
+++ trunk/ rails4scm/vendor/plugins/fs_tabpanel/test/fs_tabpanel_test.rb
@@ -1,8 +1,18 @@
 require 'test/unit'
+require 'fs_tabpanel'
 
 class FsTabpanelTest < Test::Unit::TestCase
-  # Replace this with your real tests.
+  include FsTabPanel
   def test_this_plugin
-    flunk
+    p tab_begin(['东京','tokoy',false,'罗马','roman',true,'意大利','itali',false],'one',nil);
+    p tabpage_begin('tokoy',true);
+    p tabpage_end;
+
+    p tabpage_begin('roman',false);
+    p tabpage_end;
+
+    p tabpage_begin('itali',false);
+    p tabpage_end;
+
   end
 end
