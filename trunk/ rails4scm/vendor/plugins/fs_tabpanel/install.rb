80f679e47d81f41f473f5fdb69de0e2061c3fd7a
fuxinqi@sina.com <fuxinqi@sina.com@cc345c39-9238-0410-bb01-f3a6c2eead41>
2008-12-09 07:15:23 +0000

    补上需要的位图

--- trunk/ rails4scm/vendor/plugins/fs_tabpanel/install.rb
+++ trunk/ rails4scm/vendor/plugins/fs_tabpanel/install.rb
@@ -1,1 +1,15 @@
 # Install hook code here
+require 'fileutils'
+
+def copy_files(source_path, destination_path, directory)
+  source, destination = File.join(directory, source_path), File.join(RAILS_ROOT, destination_path)
+  FileUtils.mkdir(destination) unless File.exist?(destination)
+  FileUtils.cp_r(Dir.glob(source+'/*.*'), destination)
+end
+
+Dir.chdir(Dir.getwd.sub(/vendor.*/, '')) do
+
+  directory = File.dirname(__FILE__)
+  copy_files("/public", "/public", directory)
+
+end
